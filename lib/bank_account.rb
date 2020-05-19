7abf5865b208dc2b3aa54932d475947e0e0f11d2
kiku ishimoto <kikuish@gmail.com>
2020-05-19 19:22:30 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,21 +7,21 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
-  
+
   def deposit(amount)
     @balance += amount
   end
-  
+
   def display_balance
     "Your balance is $#{balance}."
   end
-  
+
   def valid?
     if status == 'open' && balance > 0
       true
     else
       false
-    end 
+    end
   end
 
   def close_account
