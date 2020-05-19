0c1b638e421385bfb6677a3bfbaccc00cbe02ee1
kiku ishimoto <kikuish@gmail.com>
2020-05-19 19:44:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid? && receiver.valid?
+    if sender.valid? && receiver.valid? && status == pending
       @receiver.balance += @amount
       @sender.balance -= @amount
       @status = 'complete'
